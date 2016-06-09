c913f652454aebd707535c9391ca489619cb2289
Ariel Tay <shihqian@gmail.com>
2016-06-09 17:43:28 +0800

    Update Project 1A
    - Deleted the birds/bombs
    - Updated the CSS and JS file

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
+/* Global $ */
 // Create the canvas
+var healthbar = $('#lifebar');
+var healthCounter = 1000;
 
 var canvas = document.createElement('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = 1200;
 canvas.height = 800;
-//canvas.style = "border: 4px solid black; position:absolute; left: 31%; width: 1200px; height: 800px; margin-left: -250px; margin-top: 10px";
-canvas.style = " position:absolute; width: 1400px; height: 900px; margin-top: 50px; margin-left:auto; margin-right:auto; left:0; right:0; ";
-//canvas.style = 'left: 50%; margin-left: 8% ';
+// canvas.style = "border: 4px solid black; position:absolute; left: 31%; width: 1200px; height: 800px; margin-left: -250px; margin-top: 10px";
+canvas.style = ' position:absolute; width: 1400px; height: 900px; margin-top: 50px; margin-left:auto; margin-right:auto; left:0; right:0; ';
+// canvas.style = 'left: 50%; margin-left: 8% ';
 
 document.body.appendChild(canvas);
 
@@ -16,47 +19,47 @@ var bgImage = new Image();
 bgImage.onload = function () {
   bgReady = true;
 };
-bgImage.src = 'images/bg.png';
+bgImage.src = 'images/bg2.png';
 
 // Bird Images
-var BirdReady = false;
-var BirdImage = new Image();
-BirdImage.onload = function () {
-  BirdReady = true;
+var unicornReady = false;
+var unicornImage = new Image();
+unicornImage.onload = function () {
+  unicornReady = true;
 };
-BirdImage.src = 'images/bird2.png';
+unicornImage.src = 'images/unicorn.png';
 
 // Seeds Images
-var seedsReady = false;
-var seedsImage = new Image();
-seedsImage.onload = function () {
-  seedsReady = true;
+var coinReady = false;
+var coinImage = new Image();
+coinImage.onload = function () {
+  coinReady = true;
 };
-seedsImage.src = 'images/corn.png';
+coinImage.src = 'images/coin.png';
 
 // Bomb Image
-var bombReady = false;
-var bombImage = new Image();
-bombImage.onload = function () {
-  bombReady = true;
+var policeReady = false;
+var policeImage = new Image();
+policeImage.onload = function () {
+  policeReady = true;
 };
-bombImage.src = 'images/bomb2.png';
+policeImage.src = 'images/police.png';
 
-var bird = {
-  speed: 500 //movement in pixels per second
+var unicorn = {
+  speed: 600    // movement in pixels per second
 };
-//an objects of seeds
+// an objects of coins
 // var bombs = {};
-var bombArray = [];
-var seeds = {};
-var bombsCaught = false;
-var seedsCaught = 0;
+var policeArray = [];
+var coins = {};
+var policeCaught = false;
+var coinCaught = 0;
 var playerLife = 3;
 
 // Handle keyboard controls
 var keysDown = {};
 
-addEventListener("keydown", function (e) {
+addEventListener('keydown', function (e) {
   keysDown[e.keyCode] = true;
 }, false);
 
@@ -64,86 +67,118 @@ addEventListener("keyup", function (e) {
   delete keysDown[e.keyCode];
 }, false);
 
-// Reset the game when the bird eats a seed
+// Reset the game when the unicorn eats a seed
 
 function Reset () {
  // throw the monster somwehere on the screen randomly
 
-  seeds.x = 32 + (Math.random() * (canvas.width - 64));
-  seeds.y = 32 + (Math.random() * (canvas.height - 80));
+  coins.x = 32 + (Math.random() * (canvas.width - 64));
+  coins.y = 32 + (Math.random() * (canvas.height - 80));
   var bombs = {};
   bombs.x = 32 + (Math.random() * (canvas.width - 64));
   bombs.y = 32 + (Math.random() * (canvas.height - 80));
-  bombArray.push(bombs);
+  policeArray.push(bombs);
   keysDown = {};
+  UpdatedHealthBar();
+  // healthbar.width("30px");
 }
 
 function startGame () {
-  bird.x = canvas.width / 4;
-  bird.y = canvas.height / 2;
+  unicorn.x = canvas.width / 4;
+  unicorn.y = canvas.height / 2;
 }
+var flag = true;
+function MinusHealth () {
+  if(healthCounter > 0){
+    healthCounter--;
+  }
+    if(healthCounter === 0) {
+      if (flag) {
+      flag = false;
+      window.alert("Boo! Unicorn dead :( ");
+      Reset();
+      playerLife = 3;
+      coinCaught = 0;
+      keysDown = {};
+      policeArray = [];
+      healthCounter = 1000;
+    }
+   }
+console.log(healthCounter);
+   UpdatedHealthBar();
+}
+
+function UpdatedHealthBar () {
+  healthbar.width(healthCounter);
+  flag = true;
+}
+
+setInterval(MinusHealth, 10);
+//clearInterval(intervalID);
+
 
 // Update game objects
 
 function Update (modifier) {
   if (38 in keysDown) {
     // this is when player is holding up
-    bird.y -= bird.speed * modifier;
+    unicorn.y -= unicorn.speed * modifier;
     var rockTop = 0;
-    if (bird.y < rockTop) {
-      bird.y = rockTop;
+    if (unicorn.y < rockTop) {
+      unicorn.y = rockTop;
     }
   }
   if (40 in keysDown) {
     // this is when player is holding down
-    // bird.y += bird.speed * modifier;
-    bird.y += bird.speed * modifier;
+    // unicorn.y += unicorn.speed * modifier;
+    unicorn.y += unicorn.speed * modifier;
     var rockBottom = canvas.height - 60;
-    if (bird.y > rockBottom) {
-      bird.y = rockBottom;
+    if (unicorn.y > rockBottom) {
+      unicorn.y = rockBottom;
     }
   }
   if (37 in keysDown) {
     // player holding left button
-    bird.x -= bird.speed * modifier;
+    unicorn.x -= unicorn.speed * modifier;
     var rockLeft = 0;
 
-    if (bird.x < rockLeft) {
-      bird.x = rockLeft;
+    if (unicorn.x < rockLeft) {
+      unicorn.x = rockLeft;
     }
   }
   if (39 in keysDown) {
     // player holding right
-    bird.x += bird.speed * modifier;
+    unicorn.x += unicorn.speed * modifier;
     var rockRight = canvas.width - 60;
 
-    if (bird.x > rockRight) {
-      bird.x = rockRight;
+    if (unicorn.x > rockRight) {
+      unicorn.x = rockRight;
     }
   }
 
-// Have the birds ate the seeds up?
+// Have the unicorns ate the coins up?
 
   if (
-		bird.x <= (seeds.x + 32) &&
-     seeds.x <= (bird.x + 32) && bird.y <= (seeds.y + 32) && seeds.y <= (bird.y + 32)) {
-    ++seedsCaught;
+		unicorn.x <= (coins.x + 32) &&
+     coins.x <= (unicorn.x + 32) && unicorn.y <= (coins.y + 32) && coins.y <= (unicorn.y + 32)) {
+    ++coinCaught;
+   healthCounter += 10;
     Reset();
   }
-  for(var i=0; i < bombArray.length; i++){
-  if (
-		bird.x <= (bombArray[i].x + 32) &&
-     bombArray[i].x <= (bird.x + 32) && bird.y <= (bombArray[i].y + 32) && bombArray[i].y <= (bird.y + 32)) {
-       bombsCaught = true;
-    bombArray.splice(i);
-    checkBomb();
-    Reset();
+  for (var i = 0; i < policeArray.length; i++) {
+    if (
+		unicorn.x <= (policeArray[i].x + 32) &&
+     policeArray[i].x <= (unicorn.x + 32) && unicorn.y <= (policeArray[i].y + 32) && policeArray[i].y <= (unicorn.y + 32)) {
+      policeCaught = true;
+      policeArray.splice(i);
+      checkBomb();
+      Reset();
+    }
   }
-}
   // if (
-  //   bird.x <= (bombs.x + 32) &&
-  //    bombs.x <= (bird.x + 32) && bird.y <= (bombs.y + 32) && bombs.y <= (bird.y + 32)) {
-  //   ++bombsCaught;
+  //   unicorn.x <= (bombs.x + 32) &&
+  //    bombs.x <= (unicorn.x + 32) && unicorn.y <= (bombs.y + 32) && bombs.y <= (unicorn.y + 32)) {
+  //   ++policeCaught;
   //   checkBomb();
   //   Reset();
   // }
@@ -155,30 +190,30 @@ function Render () {
   if (bgReady) {
     ctx.drawImage(bgImage, 0, 0);
   }
-  if (BirdReady) {
-    ctx.drawImage(BirdImage, bird.x, bird.y);
+  if (unicornReady) {
+    ctx.drawImage(unicornImage, unicorn.x, unicorn.y);
   }
-  if (seedsReady) {
-    ctx.drawImage(seedsImage, seeds.x, seeds.y);
+  if (coinReady) {
+    ctx.drawImage(coinImage, coins.x, coins.y);
   }
-  // if (bombReady) {
-  //   ctx.drawImage(bombImage, bombs.x, bombs.y);
+  // if (policeReady) {
+  //   ctx.drawImage(policeImage, bombs.x, bombs.y);
   // }
-  for (var i = 0; i < bombArray.length; i++) {
-  ctx.drawImage(bombImage, bombArray[i].x, bombArray[i].y);
+  for (var i = 0; i < policeArray.length; i++) {
+  ctx.drawImage(policeImage, policeArray[i].x, policeArray[i].y);
   }
 
 
 
-  $('h2').text("Food Caught : " + seedsCaught);
-   if (seedsCaught <= 6){
+  $('h2').text("Food Caught : " + coinCaught);
+   if (coinCaught <= 6){
      $('#Countstatus').text("Status: I could use some food");
      }
-   else if (seedsCaught < 9)
+   else if (coinCaught < 9)
    {
-     $('#Countstatus').text("Status: Yums! Could I have more?");
+     $('#Countstatus').text('Status: Yums! Could I have more?');
    }
-   else  if (seedsCaught > 30){
+   else  if (coinCaught > 30){
      $('#Countstatus').text("Status: Thank you for the food!");
    }
    $('h3').text('Lives: ' + playerLife);
@@ -197,18 +232,22 @@ function Main () {
 }
 function checkBomb()
 {
-  if(bombsCaught){
-
-$("audio").trigger('play');
+  if (policeCaught) {
+    $('audio').trigger('play');
     playerLife--;
-   setTimeout(function () { window.alert("You're left with : " + playerLife + ' lifes'); }, 2000);
+   setTimeout(function () {
+     keysDown = {};
+     window.alert("You're left with : " + playerLife + ' lifes');
+   }, 1000);
+    healthCounter = 1000;
     //window.alert("You're left with : " + playerLife + ' lifes' );
-    bombArray = [];
+    policeArray = [];
   }
-  if(playerLife <= 0){
+  if(playerLife <= 0) {
     window.alert("You've only one thing to do, and you can't do it well at all!");
     playerLife = 3;
-    seedsCaught = 0;
+    coinCaught = 0;
+    keysDown = {};
   }
 }
 // Cross-browser support for requestAnimationFrame
